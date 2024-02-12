c83dbe3c12b2e98aca5b24ac018a230fc3b3778c
manjunathpv <manjunathpv@outlook.com>
2024-02-12 10:03:22 +0530

    initial commit

---
+++ game.py
@@ -0,0 +1,65 @@
+import pygame
+import sys
+
+from scripts.entities import PhysicsEntity
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption("Spiderman: Web Of Shadows!!")
+
+        self.screen = pygame.display.set_mode((640, 480))
+
+        self.clock = pygame.time.Clock()
+
+        # self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+        # self.img.set_colorkey((0, 0, 0))
+
+        # self.img_pos = [160, 260]
+
+        self.movement = [False, False]
+
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
+
+        self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
+
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))
+
+            self.player.update((self.movement[1] - self.movement[0]))
+
+            # img_r = pygame.Rect(
+            #     self.img_pos[0],
+            #     self.img_pos[1],
+            #     self.img.get_width(),
+            #     self.img.get_height())
+
+            # if img_r.colliderect(self.collision_area):
+            #     pygame.draw.rect(self.screen, (0, 100, 255),
+            #                      self.collision_area)
+            # else:
+            #     pygame.draw.rect(self.screen, (0, 50, 155),
+            #                      self.collision_area)
+            # self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            # self.screen.blit(self.img, self.img_pos)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    print("game exited!!")
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+            pygame.display.update()
+            self.clock.tick(60)
